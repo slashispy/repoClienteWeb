fd40061613c2817509829e4a5c38bd4ae296db37
mplanas <mplanas@visionbanco.com>
2017-11-16 17:05:11 -0300

    se formatearon algunos espacios

--- src/app/services/usuario-service.service.ts
+++ src/app/services/usuario-service.service.ts
@@ -7,15 +7,15 @@ import { Usuario } from 'app/classes/usuario';
 export class UsuarioService {
   usuarioUrl = "http://localhost:8090/rest.api/usuarios/";
 
-  constructor(private http:Http) { }
+  constructor(private http: Http) { }
 
-  getAllUsers(): Observable<Usuario[]>{
+  getAllUsers(): Observable<Usuario[]> {
     return this.http.get(this.usuarioUrl)
     .map(this.extractData)
     .catch(this.handleError);
   }
 
-  createUser(usuario: Usuario):Observable<number>{
+  createUser(usuario: Usuario): Observable<number> {
     let cpHeaders = new Headers({'Content-type' : 'application/json'});
     let options = new RequestOptions({headers : cpHeaders});
     return this.http.post(this.usuarioUrl, usuario, options)
@@ -23,12 +23,12 @@ export class UsuarioService {
     .catch(this.handleError);
   }
 
-  private extractData(res: Response | any){
+  private extractData(res: Response | any) {
     let body = res.json();
     return body;
   }
 
-  private handleError(error : Response |any){
+  private handleError(error: Response |any) {
     console.error(error.message || error);
     return Observable.throw(error.status);
   }
