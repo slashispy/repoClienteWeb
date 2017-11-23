c39157de308ee27c084005a866199caaaa0abc19
slashispy <andresplanas91@gmail.com>
2017-11-23 00:06:25 -0300

    primera version compilada correctamente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
@@ -17,6 +18,7 @@ import { UsuarioService } from './services/usuario-service.service';
     BrowserModule,
     ReactiveFormsModule,
     HttpModule,
+    CommonModule,
     RouterModule.forRoot([
     {
         path: 'usuarios',
